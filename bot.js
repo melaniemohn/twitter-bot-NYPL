f3232737ed10c521894dd99608f39957dc2ab443
Melanie Mohn <melaniemohn@yahoo.com>
2017-08-14 14:25:21 -0400

    updated config file and made related change in accessing API keys

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Twit = require('twit');
 const config = require('./config.js'); // or maybe just ./config ??
 
-const Twitter = new Twit(config);
+const Twitter = new Twit(config.twitter);
 
 // this will call the tweeter function [replace / write] every 24 hours
 // (i.e. 1000 milliseconds * 60 seconds * 60 mins * 24 hrs)
@@ -13,6 +13,23 @@ Twitter.post('statuses/update', { status: 'Oh, hey world!' }, function(err, data
   if (err) console.error(err);
   console.log(data);
 });
+
+// get initial data
+function getData() {
+	console.log('')
+	fetch('https://davidwalsh.name/some/url').then(function(response) {
+		console.log('got response: ', response);
+	})
+	// .then(function(returnedValue) {
+	// 	// ...
+	// })
+	.catch(function(err) {
+		console.error('Error getting data: ', err);
+	});
+}
+
+// getData();
+
 
 // send other tweets
 // 'tweeted' is the success callback?
