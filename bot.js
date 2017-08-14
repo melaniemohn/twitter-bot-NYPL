54bd1b47ede83eb172d33cd6643561a5b1a1c08f
Melanie Mohn <melaniemohn@yahoo.com>
2017-08-14 19:18:23 -0400

    successfully uploading and posting images

--- bot.js
+++ bot.js
@@ -9,19 +9,16 @@ const NYPLkey = config.nypl.key;
 // (i.e. 1000 milliseconds * 60 seconds * 60 mins * 24 hrs)
 // setInterval(tweeter, 1000*60*60*24);
 
-// send first tweet! for now, I'm alternating between 'Testing, testing...' and 'Oh, hey world!'
+// send first tweet! for now, alternating between 'Testing, testing...' and 'Oh, hey world!'
 Twitter.post('statuses/update', { status: 'Oh, hey world!' }, function(err, data, response) {
-  console.log('in the first post function!');
+  console.log('Posting our first Tweet...!');
   if (err) console.error(err);
-  console.log(data);
+  console.log('I just tweeted: ', data.text);
 });
-
-// save NYPL API in separate file? and export data?
 
 // use Fetch API to access NYPL API route
 function getInitialData() {
   // eventually change URL parameters to get all items in single request
-  // alternatively, could increment up to 'total pages' and interpolate string into next url
   fetch('http://api.repo.nypl.org/api/v1/items/search?q=b13668355&per_page=2', {
     headers: {
       Authorization: NYPLkey
@@ -35,20 +32,78 @@ function getInitialData() {
     return response.json();
     // note: the response from a fetch() request is a Stream object
     // calling .json() on our response object will return a Promise
-      // because the stream will be read asynchronously
     })
   .then(function(data) {
     let records = data.nyplAPI.response.result;
     // result will be an array of records
-    console.log(records[1]);
-    // return records;
+    // console.log(records);
+    return records;
   })
   .catch(function(err) {
     console.error('Error getting data: ', err);
   });
 }
 
-getInitialData();
+// getInitialData();
+
+
+// get the image URLs from getInitialData etc.
+// but need to upload the image to the media/upload endpoint, THEN post it to statuses/update
+// this requires getting the base64-encoded file content from the web image
+// create a canvas, load your image into it and then use toDataURL() to get the base64 representation ??? oy vey
+// alternatively, make an XMLHttpRequest (or fetch) and then process the response?
+// okay but I bet FileReader is a browser API? we need an alternative for node??
+// use built-in buffer function from node-fetch
+// (not clear why blob isn't working... client-side functionality, but still, should work isomorphically in node-fetch??)
+
+
+function toBase64(url) {
+  return fetch(url)
+  .then(function(response) {
+    console.log('got response from fetching image');
+    return response.buffer();
+  })
+  .then(function(buffer) {
+    // console.log('buffer?', buffer);
+    // the hardest buffer to buffer
+    return Buffer.from(buffer).toString('base64');
+  })
+  .catch(err => console.error(err));
+}
+
+
+function uploadAndPostMedia(media) {
+  Twitter.post('media/upload', { media_data: media }, function (err, data, response) {
+    if (err) console.error('Error processing upload: ', err);
+    let mediaID = data.media_id_string;
+    let altText = 'ALT TEXT HERE'; // MPM: pull alt text from image description
+    let metadata = { media_id: mediaID, alt_text: { text: altText } };
+
+    Twitter.post('media/metadata/create', metadata, function (err, data, response) {
+      if (!err) {
+        // now post a tweet with reference to the media (media will attach to the tweet)
+        var params = { status: 'Testing image upload...', media_ids: [mediaID] };
+
+        Twitter.post('statuses/update', params, function (err, data, response) {
+          if (err) console.error('Error posting status/update: ', err);
+          console.log('I just tweeted: ', data.text);
+        });
+      }
+    });
+  });
+}
+
+
+function postImageFromURL(url) {
+  toBase64(url)
+  .then(data => uploadAndPostMedia(data))
+  .catch(err => console.error(err));
+}
+
+postImageFromURL('https://images.nypl.org/index.php?id=1219221&t=w');
+
+
+// some utility functions for the future
 
 // function generateRandom() { /* . . . */ }
 
@@ -58,9 +113,3 @@ getInitialData();
 
 // function generateStreetview() { /* . . . */ }
 
-// send other tweets
-// 'tweeted' is the success callback?
-// function tweeter() {
-//   var tweet = 'do some stuff here';
-//   Twitter.post('statuses/update', { status: tweet }, tweeted);
-// }
