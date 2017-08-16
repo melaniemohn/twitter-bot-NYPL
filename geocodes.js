ef8d93f85ef4b07df77218b7e1bf59de1dbc2f4d
Melanie Mohn <melaniemohn@yahoo.com>
2017-08-16 12:36:40 -0400

    import location data into geocodes

--- geocodes.js
+++ geocodes.js
@@ -1,9 +1,10 @@
 const config = require('./config.js');
 const placesKey = config.places.key;
 
+// const locationData = require('./bot.js');
 // sometimes this is a pending Promise :/
 // could also import hard-coded data from locations.js?
-const locationData = require('./bot.js');
+const locationData = require('./locations.js').list;
 
 // note: from the docs...
 // Promises are only available if you supply a Promise constructor to the createClient() method.
@@ -26,14 +27,23 @@ const googleMapsClient = require('@google/maps').createClient({
 // iterate over this array? or just pick first item?
 // access lat-long coordinates on geometry object > location object
 
-googleMapsClient.geocode({address: '1600 Amphitheatre Parkway, Mountain View, CA'}).asPromise()
+function generateGeocode(searchString) {
+	googleMapsClient.geocode({address: searchString}).asPromise()
   .then((response) => {
     console.log('lat long coordinates:', response.json.results[0].geometry.location);
   })
   .catch((err) => {
     console.log('Error with Google Maps Client:', err);
   });
+}
 
+// for now, just try this on the first n results from NYPL
+let n = 5;
+console.log(locationData.slice(0, n));
+
+for (let i = 0; i < n; i++) {
+	generateGeocode(locationData[i]);
+}
 
 // eventually, push to some kind of data structure of geocodes
 // const geocodes = [];
